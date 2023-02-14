b52ca9815662d8a060731a2cc45b2ef3c8bd6d70
Nastya <voronkina-nastena@mail.ru>
2023-02-14 16:41:03 +0300

    make balloons on touchscreen

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+"use strict";
+
+if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+   console.log('this is a touch device');
+} else {
+   console.log('this is not a touch device');
+   document.body.classList.add('no-touch');
+}
+
+
 ymaps.ready(init);
 
 function init() {
@@ -18,7 +28,7 @@ function init() {
 
 
    var HintLayout = ymaps.templateLayoutFactory.createClass("<div class='hint'>" +
-      "{{ properties.hintContent }}" +
+      "{{ properties.content }}" +
       "</div>", {
       // Определяем метод getShape, который
       // будет возвращать размеры макета хинта.
@@ -38,53 +48,124 @@ function init() {
          }
          return result;
       }
-   }
-   );
+   });
+
+
+   var BalloonLayout = ymaps.templateLayoutFactory.createClass(
+      "<div class=\"balloon hint\">" +
+      "<div class=\"content\">$[properties.content]</div>" +
+      "<div class=\"close\"></div>" +
+      "<div class=\"tail\"></div>" +
+      "</div>", {
+
+      build: function () {
+         this.constructor.superclass.build.call(this);
+         this._$element = $('.balloon', this.getParentElement());
+         this.applyElementOffset();
+         this._$element.find('.close')
+            .on('click', $.proxy(this.onCloseClick, this));
+      },
+
+      clear: function () {
+         this._$element.find('.close')
+            .off('click');
+         this.constructor.superclass.clear.call(this);
+      },
+
+      onCloseClick: function (e) {
+         e.preventDefault();
+         this.events.fire('userclose');
+      },
+
+      applyElementOffset: function () {
+         this._$element.css({
+            left: -(this._$element[0].offsetWidth / 2),
+            top: -(this._$element[0].offsetHeight + this._$element.find('.tail')[0].offsetHeight)
+         });
+      },
+
+      getShape: function () {
+         if (!this._isElement(this._$element)) {
+            return MyBalloonLayout.superclass.getShape.call(this);
+         }
+
+         var position = this._$element.position();
+
+         return new ymaps.shape.Rectangle(new ymaps.geometry.pixel.Rectangle([
+            [position.left, position.top], [
+               position.left + this._$element[0].offsetWidth,
+               position.top + this._$element[0].offsetHeight + this._$element.find('.tail')[0].offsetHeight
+            ]
+         ]));
+      },
+
+      _isElement: function (element) {
+         return element && element[0] && element.find('.tail')[0];
+      },
+   });
+
 
    const myIcon = {
       iconLayout: 'default#image',
       iconImageHref: 'img/location/map-pin.png',
       iconImageSize: [46, 60],
       iconImageOffset: [-23, -60],
-      hintLayout: HintLayout
+      hintLayout: HintLayout,
+      balloonShadow: true,
+      balloonLayout: BalloonLayout,
+      balloonPanelMaxMapArea: 1
    };
 
    var myPlacemarks = [
       new ymaps.Placemark([59.963899, 30.300516],
-         { hintContent: 'улица Ленина, 33' }, myIcon),
+         { content: 'улица Ленина, 33' }, myIcon),
 
       new ymaps.Placemark([59.985336, 30.348378],
-         { hintContent: 'Кантемировская улица, 31' }, myIcon),
+         { content: 'Кантемировская улица, 31' }, myIcon),
 
       new ymaps.Placemark([59.931544, 30.362545],
-         { hintContent: 'Невский проспект, 118' }, myIcon),
+         { content: 'Невский проспект, 118' }, myIcon),
 
       new ymaps.Placemark([59.925232, 30.314709],
-         { hintContent: 'Садовая улица, 46' }, myIcon),
+         { content: 'Садовая улица, 46' }, myIcon),
 
       new ymaps.Placemark([59.914592, 30.350911],
-         { hintContent: 'Лиговский проспект, 130' }, myIcon),
+         { content: 'Лиговский проспект, 130' }, myIcon),
 
       new ymaps.Placemark([59.894594, 30.320180],
-         { hintContent: 'Московский проспект, 106' }, myIcon),
+         { content: 'Московский проспект, 106' }, myIcon),
 
       new ymaps.Placemark([59.944170, 30.276944],
-         { hintContent: '7-я линия Васильевского острова, 60' }, myIcon)
+         { content: '7-я линия Васильевского острова, 60' }, myIcon)
    ];
 
    var myPlaces = ymaps.geoQuery(myPlacemarks).addToMap(myMap);
 
-   for (var placemark of myPlacemarks) {
-      placemark.events
-         .add('mouseenter', function (e) {
-            e.get('target').options.set('iconImageHref', 'img/location/map-pin-hovered.png');
-         })
-         .add('mouseleave', function (e) {
-            e.get('target').options.set('iconImageHref', 'img/location/map-pin.png');
-         });
+
+
+
+   if (document.body.classList.contains('no-touch')) {
+      myMap.behaviors.enable('drag');
+      myMap.geoObjects.options.set({
+         openBalloonOnClick: false
+      });
+
+      for (var placemark of myPlacemarks) {
+         placemark.events
+            .add('mouseenter', function (e) {
+               e.get('target').options.set('iconImageHref', 'img/location/map-pin-hovered.png');
+            })
+            .add('mouseleave', function (e) {
+               e.get('target').options.set('iconImageHref', 'img/location/map-pin.png');
+            });
+      }
    }
-
-
+   else {
+      myMap.behaviors.disable('drag');
+      myMap.geoObjects.options.set({
+         showHintOnHover: false
+      });
+   }
 
    var suggestView = new ymaps.SuggestView('suggest', { boundedBy: spbRect });
 
@@ -107,7 +188,10 @@ function init() {
                myMap.setCenter(result.geometry.getCoordinates());
                myMap.setZoom(14);
 
-               result.options.set('iconImageHref', 'img/location/map-pin-hovered.png');
+               if (document.body.classList.contains('no-touch'))
+                  result.options.set('iconImageHref', 'img/location/map-pin-hovered.png');
+               else
+                  result.balloon.open();
             }
             );
       }
@@ -123,12 +207,9 @@ function init() {
    searchPlaceBtn.addEventListener('click', showNearestStore);
 
 
+
    window.addEventListener('resize', function () {
       let windowWidth = document.documentElement.clientWidth;
-      if (windowWidth <= 1140)  //tablet, mobile
-         myMap.behaviors.disable('drag');
-      else
-         myMap.behaviors.enable('drag');
 
       if (windowWidth <= 768) { //mobile
          for (var placemark of myPlacemarks)
